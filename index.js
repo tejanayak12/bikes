c9fcd287bcee32801e69683fb97e35580725fc77
Teja Nayak <prasadnaik1092@gmail.com>
2023-05-20 17:35:50 +0530

    data

--- index.js
+++ index.js
@@ -8,24 +8,24 @@ app.use(bodyparser.json());
 
 const port = 3000;
 
-const {getallbikes , getbike_id} = require ("./bikes-data/READ.js");
+const { getallbikes, getbike_id } = require("./bikes-data/READ.js");
 const insert_bikes = require("./bikes-data/CREATE.js")
 const update_bikes = require("./bikes-data/UPDATE.js");
 const delete_bikes = require("./bikes-data/DELETE");
 
-app.get("/bikes" , getallbikes);
-app.get("/bikes/:id" ,getbike_id);
-app.post("/new-bike" , insert_bikes);
-app.put("/bikes/:id" , update_bikes);
-app.delete("/bikes/:id" , delete_bikes);
+app.get("/bikes", getallbikes);
+app.get("/bikes/:id", getbike_id);
+app.post("/new-bike", insert_bikes);
+app.put("/bikes/:id", update_bikes);
+app.delete("/bikes/:id", delete_bikes);
 
-app.get("/" , (request , response) =>{
+app.get("/", (request, response) => {
     response.json({
-        status_of_server : "Running success",
-        developed_by : "TEJA NAYAK.........."
+        status_of_server: "Running success",
+        developed_by: "TEJA NAYAK.........."
     })
 })
 
-app.listen(port , () => {
+app.listen(port, () => {
     console.log("server was running..........")
 });
